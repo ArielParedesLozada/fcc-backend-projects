40c15c0915b8a9c044b0b495e814ba329410fbfa
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2026-09-21 22:06:23 -0500

    build timestamp certif

---
+++ build-a-timestamp/server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({ optionsSuccessStatus: 200 }));
+
+app.use(express.static("public"));
+
+app.get("/", (_req, res) => {
+    res.sendFile(import.meta.dirname + "/views/index.html");
+});
+
+// Do not change code above this line
+
+const getTimestamp = (req, res) => {
+    const dateParam = req.params.date
+    let date
+    if (!dateParam) {
+        date = new Date()
+
+    } else if (/^\d+$/.test(dateParam)) {
+        date = new Date(parseInt(dateParam))
+
+    } else {
+        date = new Date(dateParam)
+
+    } if (isNaN(date.getTime())) {
+        return res.json({ error: "Invalid Date" })
+    } res.json({ unix: date.getTime(), utc: date.toUTCString(), })
+
+}
+app.get("/api", getTimestamp)
+app.get("/api/:date", getTimestamp)
+
+// Do not change code below this line
+
+const PORT = 8000;
+const listener = app.listen(PORT, function () {
+    console.log("Your app is listening on port " + listener.address().port);
+});
